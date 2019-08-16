0ccbd7109ac105047d0693184748636642dd9414
Tyler Moore <TylerRMoore@gmail.com>
2019-08-15 23:03:28 -0700

    Continued work on simple example wave.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #define FASTLED_ESP8266_RAW_PIN_ORDER
 #include <FastLED.h>
-
+#include <cmath>
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
 // Move a white dot along the strip of leds.  This program simply shows how to configure the leds,
@@ -14,8 +14,8 @@
 //
 
 // How many leds are in the strip?
-#define NUM_LEDS 34
-
+#define NUM_LEDS 30
+#define MAX_BYTE 255
 // Data pin that led data will be written out over
 #define DATA_PIN 14
 
@@ -32,30 +32,63 @@ void setup() {
   Serial.begin(74880); //This is the default C++ what happens if you delete a member of an arrayhttps://docs.google.com/dCocument/d/1DzxBngHUOFZhpleMMP3qco2PPNh6JigrA3S00HgfX0Q/editefault boot output, so errors can be seen
 
   //pinMode(DATA_PIN, OUTPUT);
-  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(125);
 }
 
-bool pin_on = false;
-CRGB col = CRGB(50,50,50);
+void paint_wave(unsigned long current_millis, unsigned long start_millis, float wave_speed, float wave_width, CRGB color)
+{
+  //wave_speed is LEDs per second
+  //wave_width is LEDs
+  float last_x = wave_speed*((float)(current_millis-start_millis))/1000.0F;
+  float first_x = last_x-wave_width;
+
+  int last_LED = floor(last_x);
+  int first_LED = ceil(first_x);
+
+  Serial.println((String)first_x + "|" + (String)last_x);
+  Serial.println((String)first_LED + "|" + (String)last_LED);
+
+  if(last_LED>NUM_LEDS){last_LED=NUM_LEDS;}
+  if(first_LED<0){first_LED=0;}
+
+  for(int n=first_LED;n<=last_LED;n++)
+  {
+    float x = ((float)n-first_x);
+    //float x = ((float)n-first_x);
+    float x_percent = x/wave_width;
+    uint8_t xbyte = round(x_percent*MAX_BYTE);
+    uint8_t dim_factor = cos8(xbyte);
+
+    leds[n] = color.fadeLightBy(dim_factor);
+    //leds[n] = color; //This works perfectly, so the problem is with dimming.
+
+    Serial.print((String)dim_factor + ", ");
+  }
+  Serial.println();
+}
+
+unsigned long start_millis=5000;
+float wave_speed = 5;
+float wave_width=10;
+int test=0;
 void loop() {
-   // Move a single white led
-   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1)
-   {
+  unsigned long current_time = millis();
+  float passed = (current_time-start_millis)*wave_speed/1000.0F;
+  float total = NUM_LEDS+wave_width;
+  if(passed>total)
+  {
+    Serial.println((String)passed + "/" + (String)total);
+    Serial.println("Restarting wave.");
+    start_millis=current_time;
+  }
 
-     Serial.println("Iterating. LED " + (String)whiteLed);
-      // Turn our current led on to white, then show the leds
-      leds[whiteLed] = col;
+  for(int n=0;n<NUM_LEDS;n++)
+  {
+    leds[n] = CRGB::Black;
+  }
+  paint_wave(current_time,start_millis,wave_speed,wave_width,CRGB::Purple);
 
-      //digitalWrite(DATA_PIN,pin_on);
-      //pin_on=!pin_on;
-
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();
-
-      // Wait a little bit
-      delay(500);
-
-      // Turn our current led back to black for the next loop around
-      leds[whiteLed] = CRGB::Black;
-   }
+  FastLED.show();
+  delay(100);
 }
