b3c088b6a3c569333291d302f9dbc0aa54306ea4
Tyler Moore <TylerRMoore@gmail.com>
2019-09-27 16:58:33 -0700

    Multiple improvements to Marvel project.

--- examples/Marvel/src/main.cpp
+++ examples/Marvel/src/main.cpp
@@ -3,6 +3,12 @@
 #define START_DELAY 2000
 #define LOOP_DELAY 50
 #define SPARKLE_STEP 3
+
+#define FADE_IN_MILLIS 10000
+#define FINAL_BRIGHTNESS 100
+
+#define FIRE_FADE_IN_MILLIS 2000
+
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 //#define CLOCK_PIN 8
 
@@ -11,13 +17,13 @@ CRGB leds_left[NUM_LEDS];
 CRGB leds_right[NUM_LEDS];
 CRGB onboard_led[1];
 
-LEDGraphics::LEDSet2D left_forward(leds_left,NUM_LEDS,0,NUM_LEDS,false);
-LEDGraphics::LEDSet2D left_backward(leds_left,NUM_LEDS,NUM_LEDS,0,true);
+LEDGraphics::LEDSet2D* left_forward;
+LEDGraphics::LEDSet2D* left_backward;
 
-LEDGraphics::LEDSet2D right_forward(leds_right,NUM_LEDS,0,NUM_LEDS,false);
-LEDGraphics::LEDSet2D right_backward(leds_right,NUM_LEDS,NUM_LEDS,0,true);
+LEDGraphics::LEDSet2D* right_forward;
+LEDGraphics::LEDSet2D* right_backward;
 
-LEDGraphics::LEDSet2D onboard(onboard_led,1,0,0,true);
+LEDGraphics::LEDSet2D* onboard;
 
 LEDGraphics::LEDSet2D* left_sparkles;
 LEDGraphics::LEDSet2D* right_sparkles;
@@ -29,19 +35,22 @@ void setup() {
   Serial.begin(9600); //This is the default C++ what happens if you delete a member of an arrayhttps://docs.google.com/dCocument/d/1DzxBngHUOFZhpleMMP3qco2PPNh6JigrA3S00HgfX0Q/editefault boot output, so errors can be seen
 
   Serial.println("Starting setup.");
-
+  
   //pinMode(DATA_PIN, OUTPUT);
+  Serial.println("Adding LEDs.");
   FastLED.addLeds<WS2812, DATA_PIN_LEFT, GRB>(leds_left, NUM_LEDS);
   FastLED.addLeds<WS2812, DATA_PIN_RIGHT, GRB>(leds_right, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, ONBOARD_NEOPIXEL>(onboard_led, 1);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(0);
+  FastLED.show();
 
+  Serial.println("Creating sparkle arrays.");
   CRGB* left_sparkle_arr[NUM_LEDS];
   CRGB* right_sparkle_arr[NUM_LEDS];
   int i = 0;
   int step = NUM_LEDS/SPARKLE_STEP;
   int wrap_count = 0;
-
+  
   Serial.println("Populating sparkle arrays.");
   for(int n=0;n<NUM_LEDS;n++)
   {
@@ -56,26 +65,72 @@ void setup() {
     }
   }
 
+  Serial.println("Setting initial LED values.");
+  onboard_led[0] = CRGB(2,2,2);
+  for(int n=0;n<NUM_LEDS;n++)
+  {
+    leds_left[n] = CRGB(0,0,0);
+    leds_right[n] = CRGB(0,0,0);
+  }
+  FastLED.show();
+
+  Serial.println("Creating blue LEDSet2Ds.");
+  left_forward=new LEDGraphics::LEDSet2D(leds_left,NUM_LEDS,0,NUM_LEDS,false);
+  left_backward=new LEDGraphics::LEDSet2D(leds_left,NUM_LEDS,NUM_LEDS,0,true);
+  right_forward=new LEDGraphics::LEDSet2D(leds_right,NUM_LEDS,0,NUM_LEDS,false);
+  right_backward=new LEDGraphics::LEDSet2D(leds_right,NUM_LEDS,NUM_LEDS,0,true);
+  onboard=new LEDGraphics::LEDSet2D(onboard_led,1,0,0,true);
+  
   Serial.println("Creating sparkle LEDSet2Ds.");
   left_sparkles=new LEDGraphics::LEDSet2D(left_sparkle_arr,NUM_LEDS);
   right_sparkles=new LEDGraphics::LEDSet2D(right_sparkle_arr,NUM_LEDS);
 
+  left_forward->SetDebugName("Left Forward");
+  left_backward->SetDebugName("Right Forward");
+  right_forward->SetDebugName("Left Backward");
+  right_backward->SetDebugName("Left Backward");
+  onboard->SetDebugName("On Board");
+  left_sparkles->SetDebugName("Left Sparkles");
+  right_sparkles->SetDebugName("Right Sparkles");
+
+  Serial.println("Setting up digital inputs.");
+  pinMode(BUTTON_LEFT, INPUT_PULLUP);
+  pinMode(BUTTON_RIGHT, INPUT_PULLUP);
+
   Serial.println("Finished setup.");
 }
 
-LEDGraphics::Wave slow_half(START_DELAY,NUM_LEDS/20,NUM_LEDS/2,NUM_LEDS);
-LEDGraphics::Wave med_half(START_DELAY,NUM_LEDS/10,NUM_LEDS/2,NUM_LEDS);
-LEDGraphics::Wave med_third(START_DELAY,NUM_LEDS/10,NUM_LEDS/3,NUM_LEDS);
+LEDGraphics::Wave slow_half(0.2,NUM_LEDS*2,0.1);
+LEDGraphics::Wave med_half(0.49,NUM_LEDS,0.1);
+LEDGraphics::Wave fast_third(0.99,NUM_LEDS/2,0.1);
 
-LEDGraphics::Wave sparklewave(START_DELAY,NUM_LEDS/20,SPARKLE_STEP,NUM_LEDS);
+LEDGraphics::Hill sparklewave(10000,0.5,SPARKLE_STEP,NUM_LEDS);
 
-LEDGraphics::AddBrush blue(CRGB::Blue,0.1f);
-LEDGraphics::BlendBrush orange(CRGB::Orange,1.0f);
-CRGB background(0,0,5);
+LEDGraphics::AddBrush blue(CRGB::Blue,1.0f);
+LEDGraphics::BlendBrush orange(CRGB::DarkOrange,1.0f);
+//CRGB background(0,0,5);
+CRGB background(0,0,0);
 
 void loop() {
   unsigned long current_time = millis();
   Serial.println("Starting loop at " + (String)current_time);
+
+  if(current_time<FADE_IN_MILLIS)
+  {
+    uint8_t brightness = (float)FINAL_BRIGHTNESS*(float)current_time/(float)FADE_IN_MILLIS;
+    Serial.println("Final brightness is " + (String)brightness);
+    FastLED.setBrightness(brightness);
+  }
+
+  if(!digitalRead(BUTTON_LEFT))
+  {
+    Serial.println("Left is pushed.");
+  }
+
+  if(!digitalRead(BUTTON_RIGHT))
+  {
+    Serial.println("Right is pushed.");
+  }
 
   for(int n=0;n<NUM_LEDS;n++)
   {
@@ -83,17 +138,14 @@ void loop() {
     leds_right[n] = background;
   }
 
-  //onboard_led[0] = CRGB::Blue; //This works
-  onboard_led[0] = background; //This works
+  slow_half.Paint(current_time, left_forward, &blue);
+  slow_half.Paint(current_time, right_forward, &blue);
 
-  slow_half.Paint(current_time, &left_forward, &blue);
-  slow_half.Paint(current_time, &right_forward, &blue);
+  med_half.Paint(current_time, left_backward, &blue);
+  med_half.Paint(current_time, right_backward, &blue);
 
-  med_half.Paint(current_time, &left_backward, &blue);
-  med_half.Paint(current_time, &right_backward, &blue);
-
-  med_third.Paint(current_time, &left_forward, &blue);
-  med_third.Paint(current_time, &right_forward, &blue);
+  //fast_third.Paint(current_time, left_forward, &blue);
+  //fast_third.Paint(current_time, right_forward, &blue);
 
   sparklewave.Paint(current_time, left_sparkles, &orange);
   sparklewave.Paint(current_time, right_sparkles, &orange);
