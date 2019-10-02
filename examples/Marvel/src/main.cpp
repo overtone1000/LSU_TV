1c845549d3c8a0299ace9576626e530d6035d2e0
Tyler Moore <TylerRMoore@gmail.com>
2019-10-02 15:37:27 -0700

    More logic improvements.

--- examples/Marvel/src/main.cpp
+++ examples/Marvel/src/main.cpp
@@ -43,24 +43,24 @@ void setup() {
   delay(START_DELAY);
   Serial.begin(9600); //This is the default C++ what happens if you delete a member of an arrayhttps://docs.google.com/dCocument/d/1DzxBngHUOFZhpleMMP3qco2PPNh6JigrA3S00HgfX0Q/editefault boot output, so errors can be seen
 
-  Serial.println("Starting setup.");
+  Serial.println("1");
   
   //pinMode(DATA_PIN, OUTPUT);
-  Serial.println("Adding LEDs.");
+  Serial.println("2");
   FastLED.addLeds<WS2812, DATA_PIN_LEFT, GRB>(leds_left, NUM_LEDS);
   FastLED.addLeds<WS2812, DATA_PIN_RIGHT, GRB>(leds_right, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, ONBOARD_NEOPIXEL>(onboard_led, 1);
   FastLED.setBrightness(0);
   FastLED.show();
 
-  Serial.println("Creating sparkle arrays.");
+  Serial.println("3");
   CRGB* left_sparkle_arr[NUM_LEDS];
   CRGB* right_sparkle_arr[NUM_LEDS];
   int i = 0;
   int step = NUM_LEDS/SPARKLE_STEP;
   int wrap_count = 0;
   
-  Serial.println("Populating sparkle arrays.");
+  Serial.println("4");
   for(int n=0;n<NUM_LEDS;n++)
   {
     left_sparkle_arr[n] = leds_left+i;
@@ -74,7 +74,7 @@ void setup() {
     }
   }
 
-  Serial.println("Setting initial LED values.");
+  Serial.println("5");
   onboard_led[0] = CRGB(2,2,2);
   for(int n=0;n<NUM_LEDS;n++)
   {
@@ -83,40 +83,38 @@ void setup() {
   }
   FastLED.show();
 
-  Serial.println("Creating blue LEDSet2Ds.");
+  Serial.println("6");
   left_forward=new LEDGraphics::LEDSet2D(leds_left,NUM_LEDS,0,NUM_LEDS,false);
   left_backward=new LEDGraphics::LEDSet2D(leds_left,NUM_LEDS,NUM_LEDS,0,true);
   right_forward=new LEDGraphics::LEDSet2D(leds_right,NUM_LEDS,0,NUM_LEDS,false);
   right_backward=new LEDGraphics::LEDSet2D(leds_right,NUM_LEDS,NUM_LEDS,0,true);
   onboard=new LEDGraphics::LEDSet2D(onboard_led,1,0,0,true);
   
-  Serial.println("Creating sparkle LEDSet2Ds.");
+  Serial.println("7");
   left_sparkles=new LEDGraphics::LEDSet2D(left_sparkle_arr,NUM_LEDS);
   right_sparkles=new LEDGraphics::LEDSet2D(right_sparkle_arr,NUM_LEDS);
 
-  left_forward->SetDebugName("Left Forward");
-  left_backward->SetDebugName("Right Forward");
-  right_forward->SetDebugName("Left Backward");
-  right_backward->SetDebugName("Left Backward");
-  onboard->SetDebugName("On Board");
-  left_sparkles->SetDebugName("Left Sparkles");
-  right_sparkles->SetDebugName("Right Sparkles");
+  left_forward->SetDebugName("LF");
+  left_backward->SetDebugName("LB");
+  right_forward->SetDebugName("RF");
+  right_backward->SetDebugName("RB");
+  onboard->SetDebugName("OB");
+  left_sparkles->SetDebugName("LS");
+  right_sparkles->SetDebugName("RS");
 
-  Serial.println("Setting up digital inputs.");
+  Serial.println("8");
   pinMode(BUTTON_LEFT, INPUT_PULLUP);
   pinMode(BUTTON_RIGHT, INPUT_PULLUP);
 
-  Serial.println("Setting up mp3 player.");
+  Serial.println("9");
   mp3.showDebug(1);
   mp3.begin(9600);
   delay(500);
-
+  mp3.sendCommand(CMD_SEL_DEV,0,2);
+  delay(20);
   mp3.setVol(BASELINE_VOLUME);
 
-  
-  mp3.sendCommand(CMD_SEL_DEV,0,2);
-  
-  Serial.println("Finished setup.");
+  Serial.println("fin");
 }
 
 LEDGraphics::Wave slow_half(0.2,NUM_LEDS*2,0.1);
@@ -150,43 +148,69 @@ enum Mode
   Charging,
   Special
 };
+String mode_debug[] = {
+  "Resting",
+  "Blasting",
+  "Charging",
+  "Special"
+};
+String ModeString(Mode mode){return (String)(mode_debug[mode]);}
 
 Mode previous_mode=Resting;
 Mode current_mode=Resting;
 
-bool double_charging=false;
+bool previous_double_charging=false;
+bool current_double_charging=false;
 
-unsigned long time_previous_mode_started=0;
+unsigned long time_current_mode_started=0;
 const unsigned long theme_length=33019;
 const unsigned long blast_length=4885;
 
+unsigned long special_start=0;
+const unsigned long special_time=5000;
+
 void set_mode(Mode new_mode)
 {
+  //Don't change current mode if one of the timed modes is still running
+  switch(current_mode)
+  {
+    case Blasting:
+    {
+      if(time_current_mode_started+blast_length<current_time)
+      {
+        return;
+      }
+    }
+    break;
+    case Special:
+    {
+      if(time_current_mode_started+theme_length<current_time)
+      {
+        return;
+      }
+    }
+    default:
+    break;
+  }
   current_mode=new_mode;
-  time_previous_mode_started=millis();
+  time_current_mode_started=millis();
+  Serial.println();
+  Serial.print("Mode set to ");
+  Serial.print(ModeString(current_mode));
+  Serial.print(" at ");
+  Serial.print(time_current_mode_started);
+  Serial.println();
 }
 
 void handle_mode(unsigned long current_time)
 {
-  if(previous_mode == Blasting)
-  {
-    if(time_previous_mode_started+blast_length<current_time)
-    {
-      return;
-    }
-  }
-
   switch(current_mode)
   {
-    case Resting:
-    {
-
-    }
-    break;
     case Blasting:
     {
       if(current_mode!=previous_mode)
       {
+        Serial.println("Playing blast.");
         mp3.play(BLAST);
       }
     }
@@ -195,13 +219,23 @@ void handle_mode(unsigned long current_time)
     {
       if(current_mode!=previous_mode)
       {
+        Serial.println("Playing charging.");
         mp3.play(CHARGING);
+      }
+      if(special_start+special_time<current_time)
+      {
+        set_mode(Special);
+        return;
       }
     }
     break;
     case Special:
     {
-
+      if(current_mode!=previous_mode)
+      {
+        Serial.println("Playing Avengers theme.");
+        mp3.play(AVENGERS_THEME);
+      }
     }
     break;
   }
@@ -209,19 +243,58 @@ void handle_mode(unsigned long current_time)
   previous_mode=current_mode;
 }
 
-void set_audio()
+void button_change(unsigned long current_time)
 {
+  current_double_charging=false;
+  if(current_right_state && current_right_state!=prior_right_state)
+  {
+    set_mode(Blasting);
+  }
+  else if(current_left_state && current_left_state!=prior_left_state)
+  {
+    set_mode(Blasting);
+  }
+  else if(!current_right_state || !current_left_state)
+  {
+    set_mode(Charging);
+    if(!current_right_state && !current_left_state)
+    {
+      current_double_charging=true;
+    }
+  }
+  else
+  {
+    set_mode(Resting);
+  }
+  
 
+  if(previous_double_charging!=current_double_charging)
+  {
+    if(current_double_charging)
+    {
+      Serial.println("Increasing volume.");
+      mp3.setVol(BASELINE_VOLUME*1.5);
+
+      Serial.println("Starting special countdown.");
+      special_start=current_time;
+    }
+    else
+    {
+      Serial.println("Returning to normal volume.");
+      mp3.setVol(BASELINE_VOLUME);
+    }
+    previous_double_charging=current_double_charging;
+  }
 }
 
-void process_input()
+void process_input(unsigned long current_time)
 {
   current_left_state = digitalRead(BUTTON_LEFT);
   current_right_state = digitalRead(BUTTON_RIGHT);
 
   if(current_left_state!=prior_left_state || current_right_state!=prior_right_state)
   {
-    button_change();
+    button_change(current_time);
     if(current_left_state!=prior_left_state)
     {
       prior_left_state=current_left_state;
@@ -235,52 +308,49 @@ void process_input()
   }  
 }
 
-void button_change()
+void conditional_paint(unsigned long current_time)
 {
-  if(current_right_state && current_right_state!=prior_right_state)
+  switch(current_mode)
   {
-    set_mode(Blasting);
-  }
-  else if(current_left_state && current_left_state!=prior_left_state)
-  {
-    set_mode(Blasting);
-  }
-  else if(!current_right_state || !current_left_state)
-  {
-    set_mode(Charging);
-  }
-}
-
-void conditional_paint()
-{
-    if(!current_left_state)
-  {
-    //Left is pushed
-    float final_magnitude=(float)(current_time-left_time)/(float)FIRE_FADE_IN_MILLIS;
-    if(final_magnitude>1.0f){final_magnitude=1.0f;}
-
-    firebrush.SetMagnitude(final_magnitude);
-    for(int n=0;n<NUM_LEDS;n++)
+    case Blasting:
     {
-      firebrush.paint(leds_left+n);
-    }
-  }
-  else
-  {
 
-  }
-  
-  if(!current_right_state)
-  {
-    //Right is pushed
-    float final_magnitude=(float)(current_time-right_time)/(float)FIRE_FADE_IN_MILLIS;
-    if(final_magnitude>1.0f){final_magnitude=1.0f;}
-    
-    firebrush.SetMagnitude(final_magnitude);
-    for(int n=0;n<NUM_LEDS;n++)
-    {
-      firebrush.paint(leds_right+n);
     }
+    //break; //don't break, fall through to Charging in case the other button is still pushed
+    case Charging:
+    {
+      if(!current_left_state)
+      {
+        //Left is pushed
+        float final_magnitude=(float)(current_time-left_time)/(float)FIRE_FADE_IN_MILLIS;
+        if(final_magnitude>1.0f){final_magnitude=1.0f;}
+
+        firebrush.SetMagnitude(final_magnitude);
+        for(int n=0;n<NUM_LEDS;n++)
+        {
+          firebrush.paint(leds_left+n);
+        }
+      }
+      
+      if(!current_right_state)
+      {
+        //Right is pushed
+        float final_magnitude=(float)(current_time-right_time)/(float)FIRE_FADE_IN_MILLIS;
+        if(final_magnitude>1.0f){final_magnitude=1.0f;}
+        
+        firebrush.SetMagnitude(final_magnitude);
+        for(int n=0;n<NUM_LEDS;n++)
+        {
+          firebrush.paint(leds_right+n);
+        }
+      }
+    }
+    break;
+    case Special:
+    {
+
+    }
+    break;
   }
 }
 
@@ -289,7 +359,7 @@ void loop() {
   //mp3.play(BLAST); //Play once
 
   unsigned long current_time = millis();
-  Serial.println("Starting loop at " + (String)current_time);
+  //Serial.println("Starting loop at " + (String)current_time);
 
   if(current_time<FADE_IN_MILLIS)
   {
@@ -313,10 +383,9 @@ void loop() {
   sparklewave.Paint(current_time, left_sparkles, &orange);
   sparklewave.Paint(current_time, right_sparkles, &orange);
 
-  process_input();
+  process_input(current_time);
   handle_mode(current_time);
-  set_audio();
-  conditional_paint();
+  conditional_paint(current_time);
   
   FastLED.show();
   delay(LOOP_DELAY);
